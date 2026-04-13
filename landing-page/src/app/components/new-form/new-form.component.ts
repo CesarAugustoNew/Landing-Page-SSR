4542108ac4fba9b2e06059ace56f4d93ce3f066e
Cesar Augusto <cesarprofissional22@gmail.com>
2026-04-13 20:14:42 -0300

    feat: component form criado

---
+++ landing-page/src/app/components/new-form/new-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-form.component.html',
+  styleUrl: './new-form.component.scss'
+})
+export class NewFormComponent {
+
+}
