c33e059c782349843fc264f6ecb9213df7b5e576
Cesar Augusto <cesarprofissional22@gmail.com>
2026-04-13 21:50:08 -0300

    feat: formulario concluido

--- landing-page/src/app/components/new-form/new-form.component.ts
+++ landing-page/src/app/components/new-form/new-form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-new-form',
+  selector: 'new-form',
   standalone: true,
-  imports: [],
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
   templateUrl: './new-form.component.html',
-  styleUrl: './new-form.component.scss'
+  styleUrl: './new-form.component.scss',
 })
 export class NewFormComponent {
+  newForm!: FormGroup;
 
+  constructor() {
+    this.newForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.newForm.value);
+  }
 }
