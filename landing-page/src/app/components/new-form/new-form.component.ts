e30f7b2d4527a8a3506c72d38fe85154bd5b6899
Cesar Augusto <cesarprofissional22@gmail.com>
2026-04-13 22:38:26 -0300

    feat: implementa formulário reativo, service HTTP e integração com HttpClient

--- landing-page/src/app/components/new-form/new-form.component.ts
+++ landing-page/src/app/components/new-form/new-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import {
   FormControl,
@@ -6,18 +6,22 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
+import { sign } from 'crypto';
 
 @Component({
   selector: 'new-form',
   standalone: true,
   imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers: [NewsletterService],
   templateUrl: './new-form.component.html',
   styleUrl: './new-form.component.scss',
 })
 export class NewFormComponent {
   newForm!: FormGroup;
+  loading = signal(false);
 
-  constructor() {
+  constructor(private service: NewsletterService) {
     this.newForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -25,6 +29,15 @@ export class NewFormComponent {
   }
 
   onSubmit() {
-    console.log(this.newForm.value);
+    this.loading.set(true);
+    if (this.newForm.valid) {
+      this.service
+        .sendData(this.newForm.value.name, this.newForm.value.email)
+        .subscribe({
+          next: () => {
+            this.newForm.reset();
+          },
+        });
+    }
   }
 }
